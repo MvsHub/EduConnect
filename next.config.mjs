4ba401727453ceb389c702b9c0ebd18666dc492b
Marlon Vieira <124369161+MvsHub@users.noreply.github.com>
2025-03-18 04:29:16 +0000

    Update frontend configuration for Vercel deployment

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,10 @@ const nextConfig = {
   images: {
     domains: ['randomuser.me', 'miro.medium.com', 'vercel-blob.com'],
   },
+  // Adicione esta configuração para resolver problemas de compatibilidade
+  experimental: {
+    esmExternals: 'loose',
+  },
 };
 
 export default nextConfig;
-
